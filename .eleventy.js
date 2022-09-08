bcffc5e9873145dc6b656a81bf9d592088fb8568
Cynthia Teeters <cynthiateeters@gmail.com>
2022-09-08 16:52:10 -0400

    Add js

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/sass/");
 // Copy `img/` to `_site/img`
   eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/js");
   eleventyConfig.addWatchTarget("./src/**/*.html");
+  eleventyConfig.addWatchTarget("./src/**/*.js");
 
   return {
     dir: {
